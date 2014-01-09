46b69c84fe1a5ff3df53f6ebff56a9e759addba7
haetze <richy.sting@gmail.com>
2014-01-09 21:17:02 +0100

    fibonacci numbers

---
+++ fib.hs
@@ -0,0 +1,14 @@
+--fib.hs
+--9.1.2014
+
+import System.Environment(getArgs)
+
+
+fibStart  n = fib n 0 1 0
+fib 0 _ _ _ = 0
+fib x a b n | n<x = fib x b (a+b) (n+1)
+fib x a b n | n==x = a
+
+main = do 
+	 args <- getArgs
+         putStrLn (show (fibStart (read (head args))))
