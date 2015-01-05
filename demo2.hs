0c6a272abe4af6f6c2ea4fe1804a884f251038b7
Richard Stewing <richy.sting@gmail.com>
2015-01-05 11:15:43 -0600

    test

---
+++ demo2.hs
@@ -0,0 +1,17 @@
+#! /usr/bin/env runhugs +l
+--
+-- demo2.hs
+-- Copyright (C) 2015 haetze <haetze@ubuntu>
+--
+-- Distributed under terms of the MIT license.
+--
+--
+--
+--
+
+--test :: Num  a -> Num b 
+test a = a+1
+
+--test2 :: Num a -> Num b 
+test2 a = a/2
+
