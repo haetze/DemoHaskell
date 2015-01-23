9df440a353050e03f735b1a316bae8e0c489b2fe
Richard Stewing <richy.sting@gmail.com>
2015-01-23 02:23:37 -0600

    fib type sig

--- fib.hs
+++ fib.hs
@@ -3,8 +3,9 @@
 
 import System.Environment(getArgs)
 
-
+fibStart:: Int -> Int
 fibStart  n = fib n 0 1 0
+fib:: Int -> Int -> Int -> Int -> Int
 fib 0 _ _ _ = 0
 fib x a b n | n<x = fib x b (a+b) (n+1)
 fib x a b n | n==x = a
