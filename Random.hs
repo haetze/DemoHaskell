b260ae93879d1cacb661dbc7d8e0206f4c2adb76
Richard Stewing <richy.sting@gmail.com>
2015-04-06 13:28:34 -0500

    List in random

--- Random.hs
+++ Random.hs
@@ -11,8 +11,21 @@ module Random where
 
 import System.Random
 
-randomNumber ::Random a => (a, a) ->  IO a
+randomNumber ::Random a => (a, a) ->   IO a
 randomNumber range = do
 	g <- newStdGen
 	let [x] = take 1 $ randomRs range g 
 	return x
+
+
+
+randomNumbers ::(Random a, Num a) => [a] -> IO [a]
+randomNumbers [] = return []
+randomNumbers (_:xs) = do 
+	x <- randomNumber (0, 1000) 
+	xs <- randomNumbers xs
+	return (x:xs)
+
+nRandomNumbers::Int -> IO [Int]
+nRandomNumbers n = randomNumbers [1..n]
+
