b46e84b4b62938e2c420be3705126f43b4f04273
Richard Stewing <richy.sting@gmail.com>
2015-04-27 12:04:34 +0200

    with the number of the fib number you wanted

--- FibMain.hs
+++ FibMain.hs
@@ -18,31 +18,31 @@ main = do
 	rec chan1
 
 
-server:: Chan (Maybe Integer) -> [Int] -> IO ()
+server:: Chan (Int, (Maybe Integer)) -> [Int] -> IO ()
 server chan (x:xs) = exec chan (x:xs) [2,1,1]
 
-exec:: Chan (Maybe Integer) -> [Int] -> [Integer] -> IO ()
+exec:: Chan (Int, (Maybe Integer)) -> [Int] -> [Integer] -> IO ()
 exec chan (x:[]) ys = do 
-	writeChan chan y
-	writeChan chan $ Just (-1)
+	writeChan chan (x, y)
+	writeChan chan $ (0, Just (-1))
 	where
 		(n, zs) = fib ys x
 		y       = get zs n
 exec chan (x:xs) ys = do
-	writeChan chan y
+	writeChan chan (x, y)
 	exec chan xs zs	
 	where
 		(n, zs) = fib ys x
 		y       = get zs n
 
 
-rec:: Chan (Maybe Integer) -> IO ()
+rec:: Chan (Int, (Maybe Integer)) -> IO ()
 rec chan = do 
-	a <- readChan chan 
+	(b, a) <- readChan chan 
 	case a of
 	  Just (-1) -> putStr "done"
 	  Just n    -> do 
-		putStr $ show n ++ "\n"
+		putStr $ show b ++ ":" ++ show n ++ "\n"
 		rec chan
 	  Nothing -> do
 		putStr "failed to get index from List"
