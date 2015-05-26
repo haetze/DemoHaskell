7ba008ae726694ce83f72e526a3734985b560b33
Richard Stewing <richy.sting@gmail.com>
2015-05-26 12:21:52 +0200

    Sum to

---
+++ Sum.hs
@@ -0,0 +1,14 @@
+#! /usr/bin/env runhugs +l
+--
+-- Sum.hs
+-- Copyright (C) 2015 haetze <haetze@home>
+--
+-- Distributed under terms of the MIT license.
+--
+
+module Sum where
+
+
+sumTo a = sum [1..a]
+
+
