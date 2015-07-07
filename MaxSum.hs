a846f922a71d833375b744002006d7bb48a7cd3b
Richard Stewing <richy.sting@gmail.com>
2015-07-07 21:15:37 +0200

    finds the maximal partial sum of a list of numbers

---
+++ MaxSum.hs
@@ -0,0 +1,29 @@
+#! /usr/bin/env runhugs +l
+--
+-- MaxSum.hs
+-- Copyright (C) 2015 haetze <haetze@home>
+--
+-- Distributed under terms of the MIT license.
+--
+
+module MaxSum where
+
+
+smallList [] = []
+smallList (0:xs) = smallList xs
+smallList (x:[]) = x:[]
+smallList (x:y:xs) | x < 0 && y < 0 = smallList (x+y:xs)
+		   | x > 0 && y > 0 = smallList (x+y:xs)
+		   | otherwise 	    = x : smallList (y:xs)
+
+dropNeg [] = []
+dropNeg (x:xs) | x < 0 = xs
+	       | otherwise = (x:xs)
+
+merge [] = 0
+merge (x:[]) = x
+merge (x:y:xs) | -y < sum (y:xs) = merge (x+y:xs)
+	       |  x < merge xs 	 = merge xs
+	       | otherwise	 = x
+
+findBiggestSum = merge . dropNeg . smallList 
