5875cec3bd00ffbc7d75c4c3b8314816630d59d8
Richard Stewing <richy.sting@gmail.com>
2015-07-22 13:32:52 +0200

    added Num a typeclass constaint instaed of concrete type Double

--- MatrixMulst.hs
+++ MatrixMulst.hs
@@ -11,7 +11,7 @@ module MatrixMulst where
 
 import MatrixRot
 
-multMatrixWithMatrix:: Matrix -> Matrix -> Maybe Matrix
+multMatrixWithMatrix::Num a=> Matrix a -> Matrix a -> Maybe (Matrix a)
 multMatrixWithMatrix m n = case checkMatrix m && checkMatrix n of
 	 	True ->	Just . createMatrix $ mult j k
 		False -> Nothing
@@ -20,28 +20,30 @@ multMatrixWithMatrix m n = case checkMatrix m && checkMatrix n of
 	k = createComputeMatrix n
 
 
-computeElementFromRows:: Row -> Row -> Double
-computeElementFromRows [] [] = 0
-computeElementFromRows (x:xs) (y:ys) = x*y+computeElementFromRows xs ys
+computeElementFromRows::Num a=>Row a -> Row a -> a 
+computeElementFromRows (R []) (R []) = 0
+computeElementFromRows (R (x:xs)) (R (y:ys)) = x*y+computeElementFromRows (R xs) (R ys)
 
-mult:: ComputeMatrix -> ComputeMatrix -> ComputeMatrix
+mult::Num a => ComputeMatrix a -> ComputeMatrix a -> ComputeMatrix a
 mult xs@(ComputeMatrix x s r) ys@(ComputeMatrix y s2 r2) | r == s2 = multM xs $ rotateMatrix  ys
 		| otherwise = error "Wrong format"
 
-multM:: ComputeMatrix -> ComputeMatrix -> ComputeMatrix
+multM::Num a=> ComputeMatrix a -> ComputeMatrix a -> ComputeMatrix a
 multM a@(ComputeMatrix x s r) b@(ComputeMatrix y s2 r2) = zz
 	where 
-	e = createMatrix a
+	Matrix e = createMatrix a
 	f = createMatrix b
 	z = ff e f 
 	ff [] _ = []
 	ff (x:xs) ys = calcRow x ys : ff xs ys 
-	zz = createComputeMatrix z
+	zz = createComputeMatrix (Matrix z)
 	
-calcRow:: Row -> Matrix -> Row			
-calcRow x (y:[]) = computeElementFromRows x (reverse y) : []
-calcRow x []     = []
-calcRow x (y:ys) = computeElementFromRows x (reverse y) : calcRow x ys
+calcRow::Num a =>  Row a -> Matrix a -> Row a			
+calcRow (R x) (Matrix (R y:[])) = R $ computeElementFromRows (R x) (R (reverse y)) : []
+calcRow (R x) (Matrix [])     = R []
+calcRow (R x) (Matrix (R y:ys)) = R $ computeElementFromRows (R x) (R (reverse y)) : z
+	where
+	R z =  calcRow (R x) (Matrix ys)
 
 
 
--- MatrixRot.hs
+++ MatrixRot.hs
@@ -11,70 +11,83 @@
 module MatrixRot
 	where
 
-type Matrix = [Row]
-type Row = [Double]
+data Matrix a = Matrix [Row a]
+	deriving(Show, Read, Eq)
+data Row a = R [a]
+	deriving(Show, Read, Eq)
 	
-type M = [Element]
-type Element = (COO, Double)
+data  M a = M [Element a]
+	deriving(Show, Read, Eq)
+data Element a = E (COO, a)
+	deriving(Show, Read, Eq)
 type COO = (Int, Int)
 
-data ComputeMatrix = ComputeMatrix M Int Int
-	
-calcNewPos:: Int -> Element -> Element
-calcNewPos s ((x,y), v) = ((s-y-1, x), v)
+data ComputeMatrix a = ComputeMatrix (M a) Int Int
 
-rotateMatrix:: ComputeMatrix -> ComputeMatrix
-rotateMatrix (ComputeMatrix m s r) = ComputeMatrix (map(calcNewPos s) m)r s 
+createMatrixFromList::Num a => [[a]] -> Matrix a
+createMatrixFromList (x:xs) = Matrix $ map createRowFromList (x:xs)
+
+createRowFromList::Num a => [a] -> Row a
+createRowFromList xs = R xs
+	
+calcNewPos::Num a=> Int -> Element a -> Element a
+calcNewPos s (E ((x,y), v)) =  E ((s-y-1, x), v)
+
+rotateMatrix::Num a=> ComputeMatrix a -> ComputeMatrix a
+rotateMatrix (ComputeMatrix (M m) s r) = ComputeMatrix (M (map(calcNewPos s) m)) r s 
 
 --The Matrix is returned unmodified in case it is bad formatted
-rotateMatrix90Deg:: Matrix -> Matrix 
+rotateMatrix90Deg::Num a =>  Matrix a -> Matrix a 
 rotateMatrix90Deg x =case checkMatrix x of 
 	True ->  createMatrix . rotateMatrix $ createComputeMatrix x
 	False -> x
 
-checkMatrix:: Matrix -> Bool
-checkMatrix [[]] = False
-checkMatrix [] 	 = False
-checkMatrix (x:[]) = True
-checkMatrix (x:xs) = check xs $ length x
+checkMatrix::Num a=> Matrix a -> Bool
+checkMatrix (Matrix [R []]) = False
+checkMatrix (Matrix []) 	 = False
+checkMatrix (Matrix ( R x:[])) = True
+checkMatrix (Matrix (R x:xs)) = check xs $ length x
 	where
 	check [] _ = True
-	check (x:xs) n = length x == n && check xs n
+	check (R x:xs) n = length x == n && check xs n
 
 
-createComputeMatrix::Matrix -> ComputeMatrix
-createComputeMatrix []    = ComputeMatrix [] 0 0
-createComputeMatrix [[]] = createComputeMatrix []
-createComputeMatrix (x:xs) = ComputeMatrix z ( length (x:xs)) (length x) 
+createComputeMatrix::Num a =>Matrix a -> ComputeMatrix a
+createComputeMatrix (Matrix [])    = ComputeMatrix (M []) 0 0
+createComputeMatrix (Matrix [R []]) = createComputeMatrix (Matrix [])
+createComputeMatrix (Matrix (R x:xs)) = ComputeMatrix z ( length (R x:xs)) (length x) 
 	where
-	z = createM (0, 0) (x:xs)
+	z = createM (0, 0) (Matrix (R x:xs))
 
-createM:: COO -> Matrix -> M
-createM _	[]	= []
-createM (x, y) ([]:xs) = createM (0, y+1) xs
-createM (x, y) (z:xs) = ((x, y), head z) : createM (x+1, y) (tail z: xs)
+createM::Num a =>  COO -> Matrix a -> M a
+createM _	(Matrix [])	= M []
+createM (x, y) (Matrix (R []:xs)) = createM (0, y+1) (Matrix xs)
+createM (x, y) (Matrix ((R z):xs)) = M $ E ((x, y), (head z)) : a
+	where
+	M a = createM (x+1, y) (Matrix (R (tail z): xs)) 
 
 
-createRow:: ComputeMatrix -> Int -> [Double]
-createRow (ComputeMatrix x s r) n = row (ComputeMatrix y 1 r) 0
+createRow::Num a=>ComputeMatrix a -> Int -> Row a
+createRow (ComputeMatrix (M x) s r) n = R $ row (ComputeMatrix (M y) 1 r) 0
 	where 
 	y = filter (f n) x
-	f n ((_,y),_) = n == y
+	f n ( E ((_,y),_)) = n == y
 	
 
 
-row:: ComputeMatrix -> Int -> [Double]
+row::Num a => ComputeMatrix a -> Int -> [a]
 row (ComputeMatrix _ s r) n | n >= r = []
-row cm@(ComputeMatrix m s r) n = find m n : row cm (n+1)
+row cm@(ComputeMatrix (M m) s r) n = find (M m) n : row cm (n+1)
 	
 
-find:: M -> Int -> Double
-find [] 	_ 	= 0 
-find (((x,_),a):xs) n | x == n = a
-		| otherwise = find xs n 
+find::Num a =>  M a -> Int -> a 
+find (M []) 	_ 	= 0 
+find (M ((E ((x,_),a)):xs)) n | x == n = a
+		| otherwise = find (M xs) n 
 
-createMatrix:: ComputeMatrix -> Matrix
-createMatrix cm@(ComputeMatrix m s r) = rows cm 0
+createMatrix::Num a=> ComputeMatrix a -> Matrix a
+createMatrix cm@(ComputeMatrix (M m) s r) = rows2 cm 0
 	where
+	rows2 x n = Matrix (rows x n) 
 	rows _ n | n >= s = []
 	rows m n = createRow m n : rows m (n+1)
