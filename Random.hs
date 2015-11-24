2df5b7c0d9f948754f2c2cde1c46119113a6f7ba
Richard Stewing <richy.sting@gmail.com>
2015-11-23 18:31:06 -0800

    tabs removed

--- Random.hs
+++ Random.hs
@@ -13,9 +13,9 @@ import System.Random
 
 randomSymbole ::Random a => (a, a) ->   IO a
 randomSymbole range = do
-	g <- newStdGen
-	let [x] = take 1 $ randomRs range g 
-	return x
+  g <- newStdGen
+  let [x] = take 1 $ randomRs range g 
+  return x
 
 randomNumber:: (Random a, Num a, Show a) => (a,a) -> IO a
 randomNumber r = randomSymbole r
@@ -24,9 +24,9 @@ randomNumber r = randomSymbole r
 randomNumbers ::(Random a, Num a, Show a) => [a] -> IO [a]
 randomNumbers [] = return []
 randomNumbers (_:xs) = do 
-	x <- randomNumber (0, 10) 
-	xs <- randomNumbers xs
-	return (x:xs)
+  x <- randomNumber (0, 10) 
+  xs <- randomNumbers xs
+  return (x:xs)
 
 nRandomNumbers::Int -> IO [Int]
 nRandomNumbers n = randomNumbers [1..n]
@@ -35,7 +35,7 @@ nRandomNumbers n = randomNumbers [1..n]
 findNumber::(Num a, Eq a) => a -> [a] -> [a]
 findNumber _ [] = []
 findNumber n (x:xs) | n == x = []
-		    | n /= x = x : (findNumber n xs)
+        | n /= x = x : (findNumber n xs)
 
 findNumberInMonad:: (Num a, Eq a, Monad m) => a -> [a] -> m [a]
 findNumberInMonad n xs = return (findNumber n xs)
