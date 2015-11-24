00b14d7e2b2c9f6ed6efc3c61c681956af50866a
Richard Stewing <richy.sting@gmail.com>
2015-11-23 16:02:24 -0800

    commit

--- passwordMan.hs
+++ passwordMan.hs
@@ -16,49 +16,49 @@ import PasswordMan
 
 main:: IO ()
 main = do
-	home <- getHomeDirectory
-	checkpassWordFile home
-	args <- getArgs
-	case args of
-		("lookupUserAt":s:u:_) ->do
-			 pwd <- createPasswordsFromFileURL $ home++"/.passwords"
-			 putStr . pre $ lookupUserAtService u s pwd
-		("lookupService":s:_) ->do 
-			 pwd <- createPasswordsFromFileURL $ home++"/.passwords"
-			 shower $ lookupService s pwd
-		("showAll":_) -> do
-			 pwd <- createPasswordsFromFileURL $ home++"/.passwords"
-			 putStr $ presentAccountsInFile pwd
-		("update":s:u:p:_) -> do
-			!pwd <- createPasswordsFromFileURL $ home++"/.passwords"
-			let nP = update s u p pwd 
-			writeToDisk $ show nP
-		("insert":s:u:p:_) -> do 
-			!pwd <- createPasswordsFromFileURL $ home++"/.passwords"
-			let nP = insert s u p pwd 
-			writeToDisk $ show nP
-		("remove":s:u:_) -> do 
-			!pwd <- createPasswordsFromFileURL $ home++"/.passwords"
-			let nP = remove s u pwd 
-			writeToDisk $ show nP
-		("createPassword":_) -> do
-			p <- createStandartPassword
-			putStr $ "The password created for you is: " ++ p ++ "\n"
-		("createUser":s:u:_) -> do
-			!p <- createPasswordsFromFileURL $ home++"/.passwords"
-			nP <- createAccountForService s u p
-			writeToDisk $ show nP 
-			putStrLn . pre $ lookupUserAtService u s nP
-		_ -> putStrLn $ "Command: lookupUserAt <User> <Service> \n lookupService <Service> \n " ++
-			"update <Service> <User> <Password> \n insert <Service> <User> <Password> \n remove <Service> <User> \n showAll\n createPassword\n createUser <Service> <User>\n"
+  home <- getHomeDirectory
+  checkpassWordFile home
+  args <- getArgs
+  case args of
+    ("lookupUserAt":s:u:_) ->do
+       pwd <- createPasswordsFromFileURL $ home++"/.passwords"
+       putStr . pre $ lookupUserAtService u s pwd
+    ("lookupService":s:_) ->do 
+       pwd <- createPasswordsFromFileURL $ home++"/.passwords"
+       shower $ lookupService s pwd
+    ("showAll":_) -> do
+       pwd <- createPasswordsFromFileURL $ home++"/.passwords"
+       putStr $ presentAccountsInFile pwd
+    ("update":s:u:p:_) -> do
+      !pwd <- createPasswordsFromFileURL $ home++"/.passwords"
+      let nP = update s u p pwd 
+      writeToDisk $ show nP
+    ("insert":s:u:p:_) -> do 
+      !pwd <- createPasswordsFromFileURL $ home++"/.passwords"
+      let nP = insert s u p pwd 
+      writeToDisk $ show nP
+    ("remove":s:u:_) -> do 
+      !pwd <- createPasswordsFromFileURL $ home++"/.passwords"
+      let nP = remove s u pwd 
+      writeToDisk $ show nP
+    ("createPassword":_) -> do
+      p <- createStandartPassword
+      putStr $ "The password created for you is: " ++ p ++ "\n"
+    ("createUser":s:u:_) -> do
+      !p <- createPasswordsFromFileURL $ home++"/.passwords"
+      nP <- createAccountForService s u p
+      writeToDisk $ show nP 
+      putStrLn . pre $ lookupUserAtService u s nP
+    _ -> putStrLn $ "Command: lookupUserAt <User> <Service> \n lookupService <Service> \n " ++
+      "update <Service> <User> <Password> \n insert <Service> <User> <Password> \n remove <Service> <User> \n showAll\n createPassword\n createUser <Service> <User>\n"
 
 
 shower:: Maybe [SUP] -> IO ()
-shower Nothing 	     = putStrLn "nothing found"
+shower Nothing        = putStrLn "nothing found"
 shower (Just (x:[])) = putStr $ presentAccount x
 shower (Just (x:xs)) = do
-	putStr $ presentAccount x
-	shower $ Just xs
+  putStr $ presentAccount x
+  shower $ Just xs
 
 pre:: Maybe SUP -> String
 pre Nothing = "nothing found" 
@@ -66,16 +66,16 @@ pre (Just s) = presentAccount s
 
 writeToDisk:: String -> IO ()
 writeToDisk s = do 
-	home <- getHomeDirectory
-	writeFile (home ++ "/.passwords") s
+  home <- getHomeDirectory
+  writeFile (home ++ "/.passwords") s
 
 checkpassWordFile home = do
-	con <- getDirectoryContents home
-	case (elem ".passwords" con) of
-	 True -> return ()
-	 False -> do 
-		putStrLn "passwords file missing, a new (empty) is created"
-		createPasswordsFile home "/.passwords"
+  con <- getDirectoryContents home
+  case (elem ".passwords" con) of
+   True -> return ()
+   False -> do 
+    putStrLn "passwords file missing, a new (empty) is created"
+    createPasswordsFile home "/.passwords"
 
 
 
--- test.hs
+++ test.hs
@@ -1,41 +1,81 @@
 #! /usr/bin/env runhugs +l
 --
--- test.hs
--- Copyright (C) 2015 haetze <haetze@localhost>
+-- passwordMan.hs
+-- Copyright (C) 2015 haetze <haetze@ubuntu>
 --
 -- Distributed under terms of the MIT license.
 --
 
-newtype M a = M a
-	deriving (Show, Read)
+{-# LANGUAGE BangPatterns #-}
+
+import Data.Maybe
+import System.Environment(getArgs)
+import System.IO
+import System.Directory
+import PasswordMan
+
+main:: IO ()
+main = do
+  home <- getHomeDirectory
+  checkpassWordFile home
+  args <- getArgs
+  case args of
+    ("lookupUserAt":s:u:_) ->do
+       pwd <- createPasswordsFromFileURL $ home++"/.passwords"
+       putStr . pre $ lookupUserAtService u s pwd
+    ("lookupService":s:_) ->do 
+       pwd <- createPasswordsFromFileURL $ home++"/.passwords"
+       shower $ lookupService s pwd
+    ("showAll":_) -> do
+       pwd <- createPasswordsFromFileURL $ home++"/.passwords"
+       putStr $ presentAccountsInFile pwd
+    ("update":s:u:p:_) -> do
+      !pwd <- createPasswordsFromFileURL $ home++"/.passwords"
+      let nP = update s u p pwd 
+      writeToDisk $ show nP
+    ("insert":s:u:p:_) -> do 
+      !pwd <- createPasswordsFromFileURL $ home++"/.passwords"
+      let nP = insert s u p pwd 
+      writeToDisk $ show nP
+    ("remove":s:u:_) -> do 
+      !pwd <- createPasswordsFromFileURL $ home++"/.passwords"
+      let nP = remove s u pwd 
+      writeToDisk $ show nP
+    ("createPassword":_) -> do
+      p <- createStandartPassword
+      putStr $ "The password created for you is: " ++ p ++ "\n"
+    ("createUser":s:u:_) -> do
+      !p <- createPasswordsFromFileURL $ home++"/.passwords"
+      nP <- createAccountForService s u p
+      writeToDisk $ show nP 
+      putStrLn . pre $ lookupUserAtService u s nP
+    _ -> putStrLn $ "Command: lookupUserAt <User> <Service> \n lookupService <Service> \n " ++
+      "update <Service> <User> <Password> \n insert <Service> <User> <Password> \n remove <Service> <User> \n showAll\n createPassword\n createUser <Service> <User>\n"
 
 
-f :: Fractional b => b->b 
-f c = c+0.5
+shower:: Maybe [SUP] -> IO ()
+shower Nothing        = putStrLn "nothing found"
+shower (Just (x:[])) = putStr $ presentAccount x
+shower (Just (x:xs)) = do
+  putStr $ presentAccount x
+  shower $ Just xs
 
-g :: Num a => M a -> a
-g (M v) = v
+pre:: Maybe SUP -> String
+pre Nothing = "nothing found" 
+pre (Just s) = presentAccount s
+
+writeToDisk:: String -> IO ()
+writeToDisk s = do 
+  home <- getHomeDirectory
+  writeFile (home ++ "/.passwords") s
+
+checkpassWordFile home = do
+  con <- getDirectoryContents home
+  case (elem ".passwords" con) of
+   True -> return ()
+   False -> do 
+    putStrLn "passwords file missing, a new (empty) is created"
+    createPasswordsFile home "/.passwords"
 
 
-h:: Fractional a => M a -> a
-h a = s . f $ g a
 
-s:: Fractional a => a -> a
-s a = 1+a
-
-j:: String -> Int
-j = read
-
-k:: String -> M Int
-k = read
-
-l:: String -> Bool
-l ('M':xs) = True && l xs
-l ('m':xs) = True && l xs
-l []	   = True
-l _ 	   = False
-
-a:: Char -> String -> Bool
-a c (x:xs) | c /= x = True && a c xs
-	   | c == x = True 
-a _ [] 	   = False 
