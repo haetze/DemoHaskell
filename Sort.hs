e60b1ed18e918f90d96a7f7f64fd2c7e2ad20e7e
Richard Stewing <richy.sting@gmail.com>
2016-04-30 14:18:01 -0700

    differen sort alg.

--- Sort.hs
+++ Sort.hs
@@ -7,6 +7,40 @@
 
 module Sort where
 
+data Sorted a = Sorted (Sorted a) (Sorted a) (Sorted a) |
+                Single a |
+                None
+                deriving(Show, Read, Eq)
+
+
+insert::(Ord a) =>  a -> Sorted a -> Sorted a
+insert x (Single y) | x <  y = Sorted (Single x) None (Single y)
+                    | y <= x = Sorted (Single y) None (Single x)
+insert x None                = Single x
+insert x (Sorted (Single y) None (Single z)) | x < y =
+                                               Sorted (Single x) (Single y)
+                                                      (Single z)
+                                             | z < x =
+                                               Sorted (Single y) (Single z)
+                                                      (Single x)
+                                             | otherwise =
+                                               Sorted (Single y) (Single x)
+                                                      (Single z)
+
+insert x (Sorted a (Single y) b) | x < y     = Sorted (insert x a) (Single y) b
+                                 | otherwise = Sorted a (Single y) (insert x b)
+
+sorter:: Ord a => [a] -> Sorted a
+sorter [] = None
+sorter (x:xs) = insert x (sorter xs)
+
+sortedToList:: Sorted a -> [a]
+sortedToList None = []
+sortedToList (Single x) = [x]
+sortedToList (Sorted a b c) = sortedToList a ++ sortedToList b ++
+                              sortedToList c
+
+
 
 insertSorted x [] = [x]
 insertSorted x (y:[]) | x < y = (x:y:[])
