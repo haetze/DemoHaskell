e2d4707ee639532e634509f2d4b7132c6520bbeb
Richard Stewing <richy.sting@gmail.com>
2018-11-11 17:06:31 +0100

    primes

--- PrimeNumber.hs
+++ PrimeNumber.hs
@@ -12,11 +12,8 @@ module PrimeNumber where
 import Control.Parallel.Strategies(parMap, rpar)
 
 primesTil:: Integer -> [Integer]
-primesTil 0 = []
-primesTil 1 = []
-primesTil n = if isPrime then n:primes else primes
-  where
-    primes = primesTil $ n - 1
-    isPrime = and $ parMap rpar (\x -> n `mod` x /= 0) primes 
+primesTil n = takeWhile (<n) $ sieve [2..]
 
 isPrime n = and $ parMap rpar (\x -> n `mod` x /= 0) [2..n-1]
+
+sieve (p:ps) = p : sieve [p' | p' <- ps, p' `mod` p /= 0]
