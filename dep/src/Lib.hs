39f83e6a0ad114afc2e3c53ade7d261bdad50a9e
Richard Stewing <richy.sting@gmail.com>
2018-12-21 15:43:24 +0100

    Stack

--- dep/src/Lib.hs
+++ dep/src/Lib.hs
@@ -1,10 +1,14 @@
+{-# LANGUAGE TypeOperators #-}
 module Lib
     ( someFunc
     , myMap
+    , circ
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+type a <~ b = b -> a
 
 myMap
   :: (a -> b)
@@ -12,3 +16,9 @@ myMap
   -> [b]
 myMap _ [] = []
 myMap f (a:as) = f a : myMap f as
+
+circ
+  :: (c <~ b)
+  -> (b <~ a)
+  -> (c <~ a)
+circ = (.)  
